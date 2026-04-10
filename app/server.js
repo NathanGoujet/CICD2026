56da68f68578a9954d523d33b16c4e884cf2bf7d
Tchio Nathan <6297003@cegepmontpetit.ca>
2026-04-10 15:41:03 -0400

    modification

--- app/server.js
+++ app/server.js
@@ -4,9 +4,9 @@ const PORT = process.env.PORT || 3000;
 const ENV = process.env.APP_ENV || 'dev';
 
 const messages = {
-  dev: "Hello de l'environnement de DEV",
-  qa: "Hello de l'environnement de QA",
-  prod: "Hello de l'environnement de PROD",
+  dev: "bonjour de l'environnement de DEV",
+  qa: "bonjour de l'environnement de QA",
+  prod: "bonjour de l'environnement de PROD",
 };
 
 const message = messages[ENV] || 'Hello World';
